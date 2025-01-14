e7982c22790b1d4e101a0da452e0d528c26ff899
dvncan90 <dusan90@ymail.com>
2025-01-14 19:45:20 +0100

    Update scroll-entrance.js

--- js/scroll-entrance-master/src/scroll-entrance.js
+++ js/scroll-entrance-master/src/scroll-entrance.js
@@ -7,7 +7,7 @@
       //Set up defaults
       entrance.duration = "1500";
       entrance.distance = "500";
-      entrance.heightOffset = 500;
+      entrance.heightOffset = 0;
 
       entrance.isElemInView = function(elem) {
 
